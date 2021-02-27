e07e05b6a3a65090e8fe8d1bd749e08dc9de04b2
Dawit Ayana <dawitz28@gmail.com>
2021-02-26 16:46:27 -0800

    BinarySearch

---
+++ javascript/code-challenges/arrayBinarySearch/_test_/array-binary.test.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const binarySearch = require('../array-binary-search.js')
+
+describe('Binary Search',() => {
+    it('should return index of the array')  () => {
+        if(array )
+    })
+})
+
+
+
+
