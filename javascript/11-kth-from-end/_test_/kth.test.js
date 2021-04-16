7660def57a7027964e7f981876503308cd641103
Dawit Ayana <dawitz28@gmail.com>
2021-04-15 22:34:36 -0700

    get max first push

---
+++ javascript/11-kth-from-end/_test_/kth.test.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const kthFromEnd = require('../index.js');
+
+describe('Linked list kth from the end', () => {
+  it('should determined if k is greater than the linkedList.');
+  ('should throw an error when k and the length of the list are the same.');
+  ('should return when k is not a positibe integer');
+  ('should return when the linked list is of a size 1.');
+  ('should return when k is in the middle of the linked list.');
+
+
+});
---
+++ javascript/11-kth-from-end/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+function findNth(ll, index) {
+
+}
+
+module.exports = kthFromEnd;
